ca158d212a43430773cc5bc3f3dadde14ec370ba
Gabriel Paranhos <gbrparanhos@gmail.com>
2025-11-07 11:02:18 -0300

    adiciona sombra no nav

--- src/components/organisms/NavBar/index.tsx
+++ src/components/organisms/NavBar/index.tsx
@@ -20,8 +20,8 @@ const NavBar = () => {
 
 	const styles = {
 		container: {
-			base: 'fixed bottom-0 left-3 right-3 px-6 mb-2 flex h-14 rounded-2xl bg-[#151515] justify-between items-center z-50',
-			lg: 'lg:mb-0 lg:bottom-auto lg:px-3.5 lg:left-auto lg:right-auto lg:top-0 lg:h-20 lg:w-dvw lg:rounded-none'
+			base: 'fixed bottom-0 left-3 right-3 px-6 mb-2 flex h-14 rounded-2xl bg-[#151515] justify-between items-center z-50 shadow-[0px_0px_10px_0px_#484848]',
+			lg: 'lg:mb-0 lg:bottom-auto lg:px-3.5 lg:left-auto lg:right-auto lg:top-0 lg:h-20 lg:w-dvw lg:rounded-none lg:shadow-none'
 		},
 		text_title: {
 			base: 'font-passion font-bold text-white text-[28px] tracking-wide',
